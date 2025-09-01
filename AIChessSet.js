85408e15448b65f74505964d90f735a120d55d9b
hanfengyds <2174086642@qq.com>
2025-09-01 20:58:09 +0800

    Update AIChessSet.js

--- AIChessSet.js
+++ AIChessSet.js
@@ -1,15 +1,5 @@
 // AI对战棋子数据
 const aiPokemonData = [
- {
-        id: 'mightyena',
-        name: '大狼犬',
-        hp:4 ,
-        atk:2 ,
-        move:2 ,
-        type: 'dark',
-        typeName: '恶',
-        image: '棋子（正面）/大狼犬.gif'
- },
     {
         id: 'kyogre',
         name: '盖欧卡',
@@ -18,17 +8,27 @@ const aiPokemonData = [
         move: 2,
         type: 'water',
         typeName: '水',
-        image: '棋子（正面）/盖欧卡.gif'
+        image: '棋子（正面）/盖欧卡.gif',
     },
+{
+        id: 'mightyena',
+        name: '大狼犬',
+        hp:4 ,
+        atk:2 ,
+        move:2 ,
+        type: 'dark',
+        typeName: '恶',
+        image: '棋子（正面）/大狼犬.gif',
+ },
 {
         id: 'crobat',
         name: '叉字蝠',
         hp:4.5 ,
         atk:1.5 ,
         move:3 ,
-        type: 'flying',
-        typeName: '飞行',
-        image: '棋子（正面）/叉字蝠.gif'
+        type: ['flying','poison'],
+        typeName: ['飞行','毒'],
+        image: '棋子（正面）/叉字蝠.gif',
  },
 {
         id: 'muk',
@@ -38,7 +38,7 @@ const aiPokemonData = [
         move:1 ,
         type: 'poison',
         typeName: '毒',
-        image: '棋子（正面）/臭臭泥.gif'
+        image: '棋子（正面）/臭臭泥.gif',
  },
 {
         id: 'ludicolo',
@@ -46,9 +46,9 @@ const aiPokemonData = [
         hp:4 ,
         atk:2 ,
         move:2 ,
-        type: 'gress',
-        typeName: '草',
-        image: '棋子（正面）/乐天河童.gif'
+        type: ['grass','water'],
+        typeName: ['草','水'],
+        image: '棋子（正面）/乐天河童.gif',
  },
 {
         id: 'sharpedo',
@@ -56,9 +56,9 @@ const aiPokemonData = [
         hp:1.5 ,
         atk:2.5 ,
         move:2 ,
-        type: 'water',
-        typeName: '水',
-        image: '棋子（正面）/巨牙鲨.gif'
+        type: ['water','dark'],
+        typeName: ['水','恶'],
+        image: '棋子（正面）/巨牙鲨.gif',
  },
 ];
 
@@ -70,8 +70,8 @@ const aiLevels = [
         difficulty: '简单',
         aiPieces: aiPokemonData.slice(0, 6), // 前3个棋子
         initialPositions: [
-            { x: 2, y: 0 }, { x: 3, y: 0 }, { x: 4, y: 0 },
-            { x: 5, y: 0 }, { x: 6, y: 0 }, { x: 7, y: 0 }
+            { x: 5, y: 0 }, { x: 3, y: 0 }, { x: 7, y: 0 },
+            { x: 2, y: 0 }, { x: 6, y: 0 }, { x: 4, y: 0 }
         ]
     },
 ];
