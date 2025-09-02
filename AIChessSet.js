3b7d69b283b6086c6de6c936785c66be2e38bad8
hanfengyds <2174086642@qq.com>
2025-09-02 23:54:53 +0800

    Update AIChessSet.js

--- AIChessSet.js
+++ AIChessSet.js
@@ -80,13 +80,23 @@ const aiPokemonData = [
         typeName: ['水','恶'],
         image: 'ai-pokemon/超级巨牙鲨.gif',
  },
+     {
+        id: 'lapras',
+        name: '拉普拉斯',
+        hp:5.5,
+        atk:1.5,
+        move:1,
+        type: ['water','ice'],
+        typeName: ['水','冰'],
+        image: 'ai-pokemon/拉普拉斯.gif',
+         },
 ];
 
 // AI关卡配置
 const aiLevels = [
     {
         level: 1,
-        name: '第一关 源始的海洋',
+        name: '第一关 「源始的海洋」',
         difficulty: '简单',
         aiPieces: [
             aiPokemonData.find(p => p.id === 'kyogre'),
@@ -97,13 +107,17 @@ const aiLevels = [
             aiPokemonData.find(p => p.id === 'sharpedo-mega')
         ],
         initialPositions: [
-            { x: 5, y: 0 }, { x: 3, y: 0 }, { x: 7, y: 0 },
-            { x: 2, y: 0 }, { x: 6, y: 0 }, { x: 4, y: 0 }
+            { x: 5, y: 1 }, // 盖欧卡
+            { x: 5, y: 3 },//大狼犬
+             { x: 6, y: 0 },//叉字蝠
+            { x: 4, y: 0 }, //臭臭泥
+            { x: 6, y: 2 }, //乐天河童
+            { x: 4, y: 2 }//超级巨牙鲨
         ]
     },
     {
         level: 2,
-        name: '第二关 狂怒的海洋',
+        name: '第二关 「狂怒的海洋」',
         difficulty: '中等',
         aiPieces: [
             aiPokemonData.find(p => p.id === 'kyogre-primal'), // 原始回归盖欧卡
@@ -112,16 +126,18 @@ const aiLevels = [
             aiPokemonData.find(p => p.id === 'ludicolo'),      // 乐天河童
             aiPokemonData.find(p => p.id === 'crobat') ,  // 叉字蝠
             aiPokemonData.find(p => p.id === 'muk'),  // 臭臭泥
-            aiPokemonData.find(p => p.id === 'milotic')   // 美纳斯
+            aiPokemonData.find(p => p.id === 'milotic'),  // 美纳斯
+            aiPokemonData.find(p => p.id === 'lapras'),   // 拉普拉斯
         ],
         initialPositions: [
-            { x: 8, y: 1 },  // 原始回归盖欧卡（中心位置）
-            { x: 4, y: 4 },  // 大狼犬
+            { x: 4, y: 1 },  // 原始回归盖欧卡（中心位置）
+            { x: 4, y: 3 },  // 大狼犬
             { x: 7, y: 4 },  // 超级巨牙鲨
             { x: 2, y: 4 },// 乐天河童
-            { x: 5, y: 4 } , // 叉字蝠
-            { x: 3, y: 4 },  // 臭臭泥
+            { x: 5, y: 3 } , // 叉字蝠
+            { x: 3, y: 3 },  // 臭臭泥
             { x: 6, y: 4 },  //美纳斯
+            { x: 1, y: 4 },  //拉普拉斯
         ]
     }
 ];
